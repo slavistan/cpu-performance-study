d37485b0a916a3ce0ac506ed979e0750aced0e5c
Stanislaw Huell <huell@stud.uni-heidelberg.de>
2018-12-31 16:30:27 +0100

    finish analysis of first experiment

--- benchmark/memory-access/run.sh
+++ benchmark/memory-access/run.sh
@@ -1,4 +1,15 @@
+#!/usr/bin/env zsh
+
+source ~/.oh-my-zsh/custom/functions.zsh
+
 binary_release="../../build/memory-access"
 binary_debug="../../build_debug/memory-access"
-minstructor -o ./raw_data -f -n 25 "${binary_release} range(1,128,1)"
-minstructor -o ./raw_data -f -n 25 "${binary_debug} range(1,128,1)"
+
+raw_data_dir="./raw_data"
+csv_dir="./analysis/data.csv"
+
+cpuclk lock
+minstructor -o ${raw_data_dir} -f -n 25 "${binary_release} [1,2,4,8,16,32,64,128,256,512,1024]"
+cpuclk unlock
+mcollector -f -r -o ./analysis/data.csv ${raw_data_dir}
+
