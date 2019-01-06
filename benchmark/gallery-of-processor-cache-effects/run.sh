00360bba1e1f8820677fd52d4633b8611a965348
Stanislaw Huell <huell@stud.uni-heidelberg.de>
2019-01-06 13:34:22 +0100

    Finish l1-and-l2-cache-sizes experiment

--- benchmark/gallery-of-processor-cache-effects/run.sh
+++ benchmark/gallery-of-processor-cache-effects/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 # Source functions required to lock cpu frequency
-source ~/.oh-my-zsh/custom/functions.zsh
+source ~/.oh-my-zsh/custom/2_functions.zsh
 
 # Define binaries
 build_dir="../../build"
@@ -21,7 +21,8 @@ csv_dir="./analysis"
 ## Measurement 2
 ##
 cpuclk lock
-minstructor -o "${raw_data_dir}/l1-and-l2-cache-sizes" -f -n 25 "\"${build_dir}/l1-and-l2-cache-sizes [1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144,524288,1048576]\""
+minstructor -o "${raw_data_dir}/l1-and-l2-cache-sizes" -f -n 25 "${build_dir}/l1-and-l2-cache-sizes [1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144,524288,1048576]"
 cpuclk unlock
 mcollector -f -r -o "${csv_dir}/l1-and-l2-cache-sizes.csv" "${raw_data_dir}/l1-and-l2-cache-sizes"
+
 
