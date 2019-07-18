4a7dc4b296aea0829e03577a736ff6f3127329ca
Stanislaw Hüll <Stanislaw.Huell@js-soft.com>
2019-07-18 21:38:00 +0200

    creatae slides

--- src/col-vs-row-major/generate-report.R
+++ src/col-vs-row-major/generate-report.R
@@ -1,21 +1,23 @@
 # Generate a report from CSV data
 
 args <- commandArgs(trailingOnly = T)
-if (length(args) < 2) {
-  stop(paste0("Usage: Rscript generate-report.R ",
-               " \"/path/to/data.csv\"",
-               " \"/path/to/report.pdf\""))
-}
-
 csv.path <- args[1]
-outfile <- args[2]
 
-# TODO: Add errorbars
 library(tidyverse)
-df <- read.csv(csv.path, comment.char = '#', header = T)
-ggplot(df %>% filter(metric == "mean")) +
-  geom_line(aes(x = matrixdim, y = cpu_time, color = scheme)) +
-  scale_y_log10(breaks = 10 ^ (0:11)) +
-  scale_x_log10(breaks = 2 ^ (0:30))
+df <- read.csv(csv.path, comment.char = '#', header = T) %>% 
+  as_tibble %>%
+  select(c(scheme, matrixdim, metric, cpu_time)) %>%
+  spread(scheme, cpu_time) %>%
+  rename(colmaj_time = colmajor) %>%
+  rename(rowmaj_time = rowmajor) %>%
+  mutate(rel_time = colmaj_time / rowmaj_time) %>%
+  mutate(size_in_mega_bytes = (matrixdim ^ 2 * 8) / 1E6)
 
-ggsave(outfile)
+plt <- ggplot(df %>% filter(metric == "mean")) +
+  geom_line(aes(x = size_in_mega_bytes, y = rel_time), color="darkgreen", size=1) +
+  scale_y_continuous(limits=c(1,10), breaks=0:13, minor_breaks = c()) +
+  scale_x_log10(breaks = 10 ^ seq(-6, 14)) +
+  labs(x = "Image Size [MB]", y = "Slowdown") +
+  theme(aspect.ratio = 9/16)
+
+ggsave(args[2], height=4.5, width=8)
