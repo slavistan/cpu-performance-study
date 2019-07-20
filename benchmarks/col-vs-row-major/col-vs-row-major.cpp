b91d4131ad0b63b7ac9ee7473d4159841ec29a2f
Stanislaw Hüll <Stanislaw.Huell@js-soft.com>
2019-07-20 13:40:56 +0200

    Add maybe_unused attribute

--- benchmarks/col-vs-row-major/col-vs-row-major.cpp
+++ benchmarks/col-vs-row-major/col-vs-row-major.cpp
@@ -10,13 +10,13 @@
 #include <vector>
 
 void rowmajor(benchmark::State& state) {
-  const auto DIM = state.range(0);
-  auto vec = std::vector<double>(static_cast<std::size_t>(DIM) * DIM);
-  for (auto _: state) {
-    for(uint64_t y = 0; y < DIM; y += 1) {
-      for(uint64_t x = 0; x < DIM; x += 1) {
-        
-        const auto index = x + DIM * y;
+  const auto dim = state.range(0);
+  auto vec = std::vector<double>(uint64_t(dim) * dim);
+  for ([[maybe_unused]] auto _: state) {
+    for(uint64_t y = 0; y < dim; y += 1) {
+      for(uint64_t x = 0; x < dim; x += 1) {
+
+        const auto index = x + dim * y;
         vec[index] += 3;
       }
     }
@@ -24,20 +24,20 @@ void rowmajor(benchmark::State& state) {
 }
 
 void colmajor(benchmark::State& state) {
-  const auto DIM = state.range(0);
-  auto vec = std::vector<double>(static_cast<std::size_t>(DIM) * DIM);
-  for (auto _: state) {
-    for(uint64_t y = 0; y < DIM; y += 1) {
-      for(uint64_t x = 0; x < DIM; x += 1) {
-        
-        const auto index = y + DIM * x;
+  const auto dim = state.range(0);
+  auto vec = std::vector<double>(uint64_t(dim) * dim);
+  for ([[maybe_unused]] auto _: state) {
+    for(uint64_t y = 0; y < dim; y += 1) {
+      for(uint64_t x = 0; x < dim; x += 1) {
+
+        const auto index = y + dim * x;
         vec[index] += 3;
       }
     }
   }
 }
 
-#define MAX 15
+constexpr auto MAX = 15;
 BENCHMARK(rowmajor)->RangeMultiplier(2)->Range(8, 1 << MAX);
 BENCHMARK(colmajor)->RangeMultiplier(2)->Range(8, 1 << MAX);
 
